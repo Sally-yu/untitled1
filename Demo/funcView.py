53781ba656633aa130864130e67c3a6cd6e4942c
Yu zhaoyang <1378172370@qq.com>
2018-10-29 11:46:10 +0800

    commit

--- Demo/funcView.py
+++ Demo/funcView.py
@@ -4,9 +4,10 @@ from django.core import serializers
 from django.template import loader
 from django.views.generic import TemplateView
 from django.shortcuts import render
-from django.http import Http404,HttpResponse,JsonResponse
-from django_ajax.decorators import ajax
-
+from django.http import Http404,HttpResponse,JsonResponse,HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt,csrf_protect
+from django.core.handlers.wsgi import WSGIRequest
+from django.contrib.sessions.backends.db import SessionStore
 from Demo.models import News
 from .func import save
 
@@ -17,7 +18,7 @@ from .func import save
 def CusResponse(data):
     return JsonResponse(data,safe=False);
 
-def indexView(request):
+def indexView(request,name):
     template = loader.get_template('Demo/index.html')
     context = {
     }
@@ -57,3 +58,20 @@ def news_View(request):
     context = {
     }
     return HttpResponse(template.render(context, request))
+
+def addAdv_View(request):
+    temp=loader.get_template('Demo/addAdv.html')
+    context={}
+    return HttpResponse(temp.render(context,request))
+
+def newAdv_View(request):
+    if request.method=='POST':
+        print(request.POST)
+        print(request.session.items())
+    return HttpResponseRedirect('newAdv&%s'%(request.POST['name']))
+
+def newAdvName_View(request,name):
+    temp=loader.get_template('Demo/index.html')
+    context={'param1':name}
+    # return render(request=request,context=context,template_name=temp)
+    return HttpResponse(temp.render(context,request))
--- Demo/urls.py
+++ Demo/urls.py
@@ -9,4 +9,7 @@ urlpatterns=[
     path('savenews',views.savenews_View,name='savenews'),
     path('newslist/<int:year>',funcView.newslist_View,name='newslist'),
     path('news',views.news_View,name='news'),
+    path('addAdv',funcView.addAdv_View,name='addAdv'),
+    path('addAdv/newAdv',funcView.newAdv_View,name='newAdv'),
+    path('addAdv/newAdv&<str:name>',funcView.newAdvName_View,name='newAdv&name')
 ]
